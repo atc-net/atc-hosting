d9aac6229848190d0e50c8dbce96b61669ba6a1d
Per Kops <pko@delegate.dk>
2024-10-07 13:34:01 +0200

    chore: cleanup globalusings

--- test/Atc.Hosting.Tests/GlobalUsings.cs
+++ test/Atc.Hosting.Tests/GlobalUsings.cs
@@ -1,8 +1,6 @@
 global using System.Collections.Concurrent;
 global using System.Reflection;
-
 global using Atc.Hosting.Tests.XUnitTestTypes;
-
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Logging.Abstractions;
 global using Microsoft.Extensions.Options;
